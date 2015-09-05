2812121e78f213121c1088ac0b836a9205bea847
Michael Ziegler <diese-addy@funzt-halt.net>
2015-09-05 23:52:21 +0200

    before adding a host, check if we have it already

--- ctrl.py
+++ ctrl.py
@@ -50,26 +50,31 @@ def process_ctrl(ctrl, targets):
         target_name = data.get("name", data.get("addr", ""))
         target_addr = data.get("addr", data.get("name", ""))
         for info in socket.getaddrinfo(target_addr, 0, socket.AF_INET, socket.SOCK_STREAM):
-            # try to avoid pid collisions
-            tgt_id = randint(0x8000 + 1, 0xFFFF)
-            while tgt_id in targets:
+            # dubs. check'em.
+            for key, tgt in targets.items():
+                if tgt["addr"] == info[4][0]:
+                    break
+            else:
+                # try to avoid pid collisions
                 tgt_id = randint(0x8000 + 1, 0xFFFF)
-            tgt = {
-                "addr": info[4][0],
-                "name": target_name,
-                "sent": 0,
-                "recv": 0,
-                "errs": 0,
-                "outd": 0,
-                "id":   tgt_id,
-                "last": 0,
-                "avg":  0,
-                "min":  0,
-                "max":  0,
-                "itv":  data.get("itv", 1),
-                "due":  0
-            }
-            targets[tgt_id] = tgt
+                while tgt_id in targets:
+                    tgt_id = randint(0x8000 + 1, 0xFFFF)
+                tgt = {
+                    "addr": info[4][0],
+                    "name": target_name,
+                    "sent": 0,
+                    "recv": 0,
+                    "errs": 0,
+                    "outd": 0,
+                    "id":   tgt_id,
+                    "last": 0,
+                    "avg":  0,
+                    "min":  0,
+                    "max":  0,
+                    "itv":  data.get("itv", 1),
+                    "due":  0
+                }
+                targets[tgt_id] = tgt
         ctrl.sendto('{"status": "mkay"}', addr)
 
     elif data["cmd"] == "remove":
