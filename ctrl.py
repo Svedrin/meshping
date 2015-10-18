e26a8dd9dd33f90db80b61fd491d4f14244e0e87
Michael Ziegler <diese-addy@funzt-halt.net>
2015-10-18 14:18:18 +0200

    refactor meshping in object-oriented fashion, move the actual pinging into a separate thread using Queues for comms to get rid of timing issues

--- ctrl.py
+++ ctrl.py
@@ -10,8 +10,8 @@ import json
 from random import randint
 from time import sleep, time
 
-def process_ctrl(ctrl, targets, pingobj):
-    rdy_read, _, _ = select.select([ctrl], [], [], 0)
+def process_ctrl(ctrl, mp):
+    rdy_read, _, _ = select.select([ctrl], [], [], 0.5)
     if ctrl not in rdy_read:
         return # nothing to do
 
@@ -30,18 +30,9 @@ def process_ctrl(ctrl, targets, pingobj):
         ctrl.sendto('{"status": "ok"}', addr)
 
     elif data["cmd"] == "list":
-        ctrl.sendto(json.dumps(targets), addr)
+        ctrl.sendto(json.dumps(mp.targets), addr)
         if data.get("reset", False):
-            for tgt in targets.values():
-                tgt.update({
-                    "sent": 0,
-                    "recv": 0,
-                    "lost": 0,
-                    "last": 0,
-                    "sum":  0,
-                    "min":  0,
-                    "max":  0,
-                })
+            mp.reset_stats()
 
     elif data["cmd"] == "add":
         if "name" not in data and "addr" not in data:
@@ -52,13 +43,10 @@ def process_ctrl(ctrl, targets, pingobj):
         target_addr = data.get("addr", data.get("name", ""))
 
         try:
-            addrs = socket.getaddrinfo(target_addr, 0, 0, socket.SOCK_STREAM)
+            mp.add_host(target_name, target_addr)
         except socket.gaierror, err:
             ctrl.sendto('{"status": "error", "errmessage": "' + err.args[1] + '"}', addr)
             return
-
-        for info in addrs:
-            pingobj.add_host(info[4][0])
 
         ctrl.sendto('{"status": "ok"}', addr)
 
@@ -71,15 +59,10 @@ def process_ctrl(ctrl, targets, pingobj):
         target_addr = data.get("addr", data.get("name", ""))
 
         try:
-            addrs = socket.getaddrinfo(target_addr, 0, 0, socket.SOCK_STREAM)
+            mp.remove_host(target_name, target_addr)
         except socket.gaierror, err:
             ctrl.sendto('{"status": "error", "errmessage": "' + err.args[1] + '"}', addr)
             return
-
-        for info in addrs:
-            pingobj.remove_host(info[4][0])
-            if info[4][0] in targets:
-                del targets[info[4][0]]
 
         ctrl.sendto('{"status": "ok"}', addr)
 
--- meshping.py
+++ meshping.py
@@ -13,55 +13,100 @@ try:
 except ImportError:
     import json
 
+from threading import Thread
 from random import randint
 from oping import PingObj
 from time import sleep, time
 from optparse import OptionParser
+from Queue import Queue, Empty
 
 from ctrl import process_ctrl
 
-def main():
-    if os.getuid() != 0:
-        raise RuntimeError("need to be root, sorry about that")
+class MeshPing(object):
+    def __init__(self, interval=30):
+        self.addq = Queue()
+        self.remq = Queue()
+        self.rstq = Queue()
+        self.targets = {}
+        self.interval = interval
 
-    ctrl = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.SOL_UDP)
-    ctrl.bind(("127.0.0.1", 55432))
+        self.pingdaemon = Thread(target=self.ping_daemon_runner)
+        self.pingdaemon.daemon = True
 
-    targets = {}
+    def start(self):
+        self.pingdaemon.start()
 
-    parser = OptionParser("Usage: %prog [options] <target ...>")
-    parser.add_option(
-        "-i", "--interval", help="Interval in which pings are sent", type=int, default=30
-    )
-    options, posargs = parser.parse_args()
+    def is_alive(self):
+        return self.pingdaemon.is_alive()
 
-    pingobj = PingObj()
+    def add_host(self, name, addr):
+        for info in socket.getaddrinfo(addr, 0, 0, socket.SOCK_STREAM):
+            if info[4][0] not in self.targets:
+                self.addq.put((name, info[4][0]))
 
-    for target in posargs:
-        for info in socket.getaddrinfo(target, 0, 0, socket.SOCK_STREAM):
-            pingobj.add_host(info[4][0])
+    def remove_host(self, name, addr):
+        for info in socket.getaddrinfo(addr, 0, 0, socket.SOCK_STREAM):
+            self.remq.put((name, info[4][0]))
 
-    try:
+    def reset_stats(self):
+        self.rstq.put(True)
+
+    def reset_stats_for_target(self, addr):
+        self.targets[addr].update({
+            "sent": 0,
+            "lost": 0,
+            "recv": 0,
+            "last": 0,
+            "sum":  0,
+            "min":  0,
+            "max":  0
+        })
+
+
+    def ping_daemon_runner(self):
+        pingobj = PingObj()
+
+        next_ping = time() + 0.1
+
         while True:
-            now = time()
-            next_ping = now + options.interval
+            while time() < next_ping:
+                # Process Host Add/Remove queues
+                try:
+                    while True:
+                        name, addr = self.addq.get(timeout=0.1)
+                        pingobj.add_host(addr)
+                        self.targets[addr] = {
+                            "name": name,
+                            "addr": addr
+                        }
+                        self.reset_stats_for_target(addr)
+                except Empty:
+                    pass
 
-            process_ctrl(ctrl, targets, pingobj)
+                try:
+                    while True:
+                        name, addr = self.remq.get(timeout=0.1)
+                        pingobj.remove_host(addr)
+                        if addr in self.targets:
+                            del self.targets[addr]
+                except Empty:
+                    pass
+
+                try:
+                    self.rstq.get(timeout=0.1)
+                except Empty:
+                    pass
+                else:
+                    for addr in self.targets:
+                        self.reset_stats_for_target(addr)
+
+            now = time()
+            next_ping = now + self.interval
 
             pingobj.send()
 
             for hostinfo in pingobj.get_hosts():
-                target = targets.setdefault(hostinfo["addr"], {
-                    "name": hostinfo["name"],
-                    "addr": hostinfo["addr"],
-                    "sent": 0,
-                    "lost": 0,
-                    "recv": 0,
-                    "last": 0,
-                    "sum":  0,
-                    "min":  0,
-                    "max":  0
-                })
+                target = self.targets[hostinfo["addr"]]
 
                 target["sent"] += 1
 
@@ -85,11 +130,38 @@ def main():
                         # can happen if sent is reset after a ping has been sent out, but before its answer arrives
                         target["sent"] = target["lost"]
 
-            if time() < next_ping:
-                sleep(next_ping - time())
+
+
+def main():
+    if os.getuid() != 0:
+        raise RuntimeError("need to be root, sorry about that")
+
+    ctrl = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.SOL_UDP)
+    ctrl.bind(("127.0.0.1", 55432))
+
+    parser = OptionParser("Usage: %prog [options] <target ...>")
+    parser.add_option(
+        "-i", "--interval", help="Interval in which pings are sent", type=int, default=30
+    )
+    options, posargs = parser.parse_args()
+
+    mp = MeshPing(options.interval)
+
+    for target in posargs:
+        mp.add_host(target, target)
+
+    try:
+        mp.start()
+
+        while mp.is_alive():
+            process_ctrl(ctrl, mp)
 
     except KeyboardInterrupt:
         pass
+
+    finally:
+        del mp
+        ctrl.close()
 
 if __name__ == '__main__':
     main()
