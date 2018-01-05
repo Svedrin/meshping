f2c4df27714d3e0a16fdf418917ca5a491ba53c7
Michael Ziegler <michael.ziegler@proemion.com>
2018-01-05 22:50:04 +0100

    update mpcli to use redis 'n stuff

--- cli.py
+++ cli.py
@@ -5,137 +5,40 @@
 from __future__ import division
 
 import sys
-import json
-import struct
 import socket
 
+from redis import StrictRedis
 from optparse import OptionParser
-from operator import itemgetter
-from select   import select
 
 def main():
     parser = OptionParser(usage="Usage: %prog [options] -- no options = list")
 
-    parser.add_option("-j", "--json",      help="Output the reply", default=False, action="store_true")
     parser.add_option("-d", "--delete",    help="remove target",    default=False, action="store_true")
     parser.add_option("-a", "--add",       help="add target",       default=False, action="store_true")
-    parser.add_option("-H", "--histogram", help="show histogram",   default=False, action="store_true")
-    parser.add_option("-t", "--name",      help="target name",      default="")
-    parser.add_option("-T", "--address",   help="target address",   default="")
-    parser.add_option("-i", "--interval",  help="ping interval",    type=int, default=30)
-    parser.add_option("-A", "--addscript", help="generate host add script for the currently configured hosts", default=False, action="store_true")
-
+    parser.add_option("-r", "--redishost", help="Redis Host [127.0.0.1]", default="127.0.0.1")
     options, posargs = parser.parse_args()
 
-    ctrl = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.SOL_UDP)
+    redis = StrictRedis(host=options.redishost)
 
     if options.add:
-        opts = {
-            "cmd":    "add",
-            "itv":    options.interval,
-        }
-        if options.name:
-            opts["name"] = options.name
-        if options.address:
-            opts["addr"] = options.address
-        ctrl.sendto(json.dumps(opts), ("127.0.0.1", 55432) )
+        for target in posargs:
+            if "@" not in target:
+                for info in socket.getaddrinfo(target, 0, 0, socket.SOCK_STREAM):
+                    redis.sadd("meshping:targets", "%s@%s" % (target, info[4][0]))
+            else:
+                redis.sadd("meshping:targets", target)
 
     elif options.delete:
-        opts = {
-            "cmd":    "remove",
-            "itv":    options.interval,
-        }
-        if options.name:
-            opts["name"] = options.name
-        if options.address:
-            opts["addr"] = options.address
-        ctrl.sendto(json.dumps(opts), ("127.0.0.1", 55432) )
-
-    elif options.histogram:
-        opts = {
-            "cmd":    "histogram",
-        }
-        if options.address:
-            opts["addr"] = options.address
-        ctrl.sendto(json.dumps(opts), ("127.0.0.1", 55432) )
+        for target in posargs:
+            if "@" not in target:
+                for info in socket.getaddrinfo(target, 0, 0, socket.SOCK_STREAM):
+                    redis.srem("meshping:targets", "%s@%s" % (target, info[4][0]))
+            else:
+                redis.srem("meshping:targets", target)
 
     else:
-        ctrl.sendto( json.dumps({
-            "cmd":    "list",
-        }), ("127.0.0.1", 55432) )
-
-    rdy_read, _, _ = select([ctrl], [], [], 5)
-    if ctrl in rdy_read:
-        reply, addr = ctrl.recvfrom(2**14)
-
-        if options.json:
-            print json.dumps(json.loads(reply), indent=4)
-
-        elif options.add or options.delete:
-            pass # no output wanted
-
-        elif options.histogram:
-            histogram = json.loads(reply)
-            base = 2
-
-            if "status" in histogram:
-                print >> sys.stderr, histogram["status"]
-                return 1
-
-            # json sucks and converts dict keys to strings
-            histogram = dict([(int(x), y) for (x, y) in histogram.items()])
-
-            # Let's try a modality detection
-            # http://www.brendangregg.com/FrequencyTrails/modes.html
-            last    = None
-            mvalue  = 0
-            maxnum  = None
-
-            for bktval, count in sorted(histogram.items(), key=itemgetter(0), reverse=True):
-                print "%7.2f - %7.2f -> %5d %s" % ( base ** (bktval / 10.), base ** ((bktval + 1) / 10.), count, (u"■".encode("utf-8")) * count )
-                if last is not None:
-                    mvalue += abs(count - last)
-                    maxnum  = max(maxnum, count)
-                last = count
-
-            if maxnum is not None:
-                mvalue /= maxnum
-                print "%d buckets, mvalue=%.2f (probably %s multimodal)" % (len(histogram), mvalue, "is" if mvalue > 2.4 else "not")
-
-        else:
-            targets = json.loads(reply)
-
-            if options.addscript:
-                for targetinfo in targets.values():
-                    print "%s -a -t %s -T %s" % (sys.argv[0], targetinfo["name"], targetinfo["addr"])
-                return
-
-            print "Target                     Sent  Recv   Succ    Loss      Min       Avg       Max      Last"
-
-            def ip_as_int(tgt):
-                if tgt["af"] == socket.AF_INET:
-                    return struct.unpack("!I", socket.inet_aton( tgt["addr"] ))[0]
-                elif tgt["af"] == socket.AF_INET6:
-                    ret = 0
-                    for intpart in struct.unpack("!IIII", socket.inet_pton(socket.AF_INET6, tgt["addr"] )):
-                        ret = ret<<32 | intpart
-                    return ret
-
-            for targetinfo in sorted(targets.values(), key=ip_as_int):
-                loss = 0
-                errs = 0
-                if targetinfo["sent"]:
-                    loss = (targetinfo["sent"] - targetinfo["recv"]) / targetinfo["sent"] * 100
-                avg = 0
-                if targetinfo["recv"]:
-                    avg = targetinfo["sum"] / targetinfo["recv"]
-                outd = 0
-                print "%-25s %5d %5d %6.2f%% %6.2f%% %7.2f   %7.2f   %7.2f   %7.2f" % (
-                    targetinfo["addr"], targetinfo["sent"], targetinfo["recv"], 100 - loss, loss,
-                    targetinfo["min"], avg, targetinfo["max"], targetinfo["last"])
-
-    else:
-        print "timeout, is meshping running?"
+        for target in redis.smembers("meshping:targets"):
+            print target
 
 
 if __name__ == '__main__':
