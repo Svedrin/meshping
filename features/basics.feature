4f8e5a54cf86afe037e0491b300d0a729dc2957c
Michael Ziegler <michael.ziegler@proemion.com>
2020-06-30 20:48:46 +0200

    add basic tests

--- features/basics.feature
+++ features/basics.feature
@@ -1,6 +1,24 @@
 Feature: Basic stuff.
 
-  Scenario: Add a target.
+  Scenario: Add a target through the UI endpoints.
 
+     when we request a histogram for target "8.8.8.8"
+     then we get a response with status code 404
      when we add a target of "8.8.8.8" named "google.com"
      then there exists a target of "8.8.8.8" named "google.com"
+     when we wait 2 seconds
+     when we request a histogram for target "8.8.8.8"
+     then we get a response with status code 200
+     when we delete a target of "google.com@8.8.8.8"
+     #when we delete a target of "8.8.8.8"
+     then there exists no target of "8.8.8.8"
+
+  Scenario: Same thing through the peering endpoints.
+
+  Scenario: Add a target through the UI endpoints and see that it gets dist'ed to peers.
+
+    These targets are NOT foreign, thus shall be distributed.
+
+  Scenario: Add a target through the peering endpoints and see that it does NOT get dist'ed to peers.
+
+    These targets are foreign, thus shall NOT be distributed.
--- features/steps/basics.py
+++ features/steps/basics.py
@@ -3,7 +3,12 @@
 import json
 import requests
 
+from time import sleep
 from behave import given, when, then
+
+@when('we wait {n:d} seconds')
+def step(context, n):
+    sleep(n)
 
 @when('we add a target of "{address}" named "{name}"')
 def step(context, address, name):
@@ -19,6 +24,12 @@ def step(context, address, name):
     resp.raise_for_status()
     assert resp.json()["success"] == True
 
+@when('we delete a target of "{address}"')
+def step(context, address):
+    resp = requests.delete("http://meshping:9922/api/targets/%s" % address)
+    resp.raise_for_status()
+    assert resp.json()["success"] == True
+
 @then('there exists a target of "{address}" named "{name}"')
 def step(context, address, name):
     resp = requests.get("http://meshping:9922/api/targets")
@@ -29,3 +40,20 @@ def step(context, address, name):
             break
     else:
         assert False, "target does not exist"
+
+@then('there exists no target of "{address}"')
+def step(context, address):
+    resp = requests.get("http://meshping:9922/api/targets")
+    resp.raise_for_status()
+    assert resp.json()["success"] == True
+    for target in resp.json()["targets"]:
+        if target["addr"] == address and target["name"] == name:
+            assert False, "target exists (it shouldn't)"
+
+@when('we request a histogram for target "{address}"')
+def step(context, address):
+    context.resp = requests.get("http://meshping:9922/histogram/obsolete/%s.png" % address)
+
+@then('we get a response with status code {status:d}')
+def step(context, status):
+    assert context.resp.status_code, status
