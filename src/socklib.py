f9dd43b261a6f736f834a6faf6b0bbf563cd9ab9
Michael Ziegler <diese-addy@funzt-halt.net>
2024-08-01 23:56:17 +0200

    we need to send ICMP packets to actually get a meaningful answer, so let's do that

--- src/socklib.py
+++ src/socklib.py
@@ -1,15 +1,22 @@
 import socket
 
+from icmplib.sockets import ICMPv4Socket, ICMPSocketError
+from icmplib.models  import ICMPRequest
+
 # see /usr/include/linux/in.h
 IP_MTU_DISCOVER = 10
 IP_PMTUDISC_DO  =  2
 IP_MTU          = 14
+IP_HEADER_LEN   = 20
+ICMP_HEADER_LEN =  8
 
 # see /usr/include/linux/in6.h
 IPV6_MTU_DISCOVER = 23
 IPV6_PMTUDISC_DO  =  2
 IPV6_MTU          = 24
+IPV6_HEADER_LEN   = 40
 
+UDP_HEADER_LEN    =  8
 
 def reverse_lookup(ip):
     try:
@@ -18,18 +25,45 @@ def reverse_lookup(ip):
         return ip
 
 
-def ipv4_pmtud(ip):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.IPPROTO_IP, IP_MTU_DISCOVER, IP_PMTUDISC_DO)
-    s.connect((ip, 99))
+class PMTUDv4Socket(ICMPv4Socket):
+    def _create_socket(self, type):
+        sock = super(PMTUDv4Socket, self)._create_socket(type)
+        sock.setsockopt(socket.IPPROTO_IP, IP_MTU_DISCOVER, IP_PMTUDISC_DO)
+        return sock
 
-    try:
-        # deliberately send a way-too-large packet to provoke an error
-        s.send(b'#' * 9999)
-    except socket.error:
-        return s.getsockopt(socket.IPPROTO_IP, IP_MTU)
-    else:
-        raise ValueError("Path MTU not found")
+    def get_mtu(self):
+        return self._sock.getsockopt(socket.IPPROTO_IP, IP_MTU)
+
+    def send(self, request):
+        self._sock.connect((request.destination, 0))
+        return super(PMTUDv4Socket, self).send(request)
+
+def ipv4_pmtud(ip):
+    mtu = 9999
+    with PMTUDv4Socket(address=None, privileged=True) as sock:
+        for sequence in range(30):
+            request = ICMPRequest(
+                destination  = ip,
+                id           = 1337,
+                sequence     = sequence,
+                payload_size = mtu - IP_HEADER_LEN - ICMP_HEADER_LEN
+            )
+            try:
+                # deliberately send a way-too-large packet to provoke an error
+                sock.send(request)
+                sock.receive(request, 1)
+                break
+            except ICMPSocketError as err:
+                print("nay! %r" % err)
+                if "Message too long" not in err.message:
+                    raise
+
+            mtu = sock.get_mtu()
+            print("new mtu!", mtu)
+            continue
+
+    print("done:", mtu)
+    return mtu
 
 
 def ipv6_pmtud(ip):
