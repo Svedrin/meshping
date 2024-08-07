2ed572a7c29ad7c285847027b1d8120df42baf06
Michael Ziegler <diese-addy@funzt-halt.net>
2024-08-07 07:41:45 +0200

    need more than one single hop for a loop to occur

--- src/meshping.py
+++ src/meshping.py
@@ -69,8 +69,9 @@ class MeshPing:
                 )
 
                 hopaddrs = [hop.address for hop in trace]
+                hoaddrs_set = set(hopaddrs)
                 target.set_route_loop(
-                    len(hopaddrs) != len(set(hopaddrs))
+                    len(hopaddrs) != len(hoaddrs_set) and len(hoaddrs_set) > 1
                 )
 
                 trace_hops = []
